4f9a780dc81cf5dc5d387b109531e9f32c0af206
Zhi Li <lizhieffe@gmail.com>
2015-03-16 00:51:27 -0400

    zhat iOS client

---
+++ client-iOS/Zhat/Zhat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Zhat
+//
+//  Created by Zhi Li on 3/16/15.
+//  Copyright (c) 2015 Zhi Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
